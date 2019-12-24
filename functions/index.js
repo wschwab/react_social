3b05d56073a017ccbd6df2c7ccf2bb141d4213ef
wschwab <x@x.x>
2019-12-24 20:45:35 +0200

    first commit

---
+++ functions/index.js
@@ -0,0 +1,47 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/posts', (req, res) => {
+    admin
+        .firestore()
+        .collection('posts')
+        .get()
+        .then(data => {
+            let posts = [];
+            data.forEach(doc => {
+                posts.push(doc.data());
+            });
+            return res.json(posts);
+        })
+    .catch(err => console.error(err));
+})
+
+
+exports.createPost = functions.https.onRequest((req, res) => {
+        if(req.method !== 'POST'){
+            return res.status(400).json({ error: "Method not allowed" })
+        }
+
+        const newPost = {
+            body: req.body.body,
+            user: req.body.userHandle,
+            createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        };
+
+        admin
+            .firestore()
+            .collection('posts')
+            .add(newPost)
+            .then(doc => {
+                res.json({ message: `document ${doc.id} created successfully` });
+            })
+            .catch(err =>{
+                res.status(500).json({ error: 'something went wrong' });
+                console.error(err);
+            });
+});
