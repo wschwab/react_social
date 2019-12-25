14d582dad7a181564bd2f2c24200e77293d093ec
wschwab <x@x.x>
2019-12-25 14:18:14 +0200

    signup (auth)

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,26 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyC5HD7k8Z-qobxB2Dtu7oDJsofW93xTYx0",
+    authDomain: "reactsocial-dfbaf.firebaseapp.com",
+    databaseURL: "https://reactsocial-dfbaf.firebaseio.com",
+    projectId: "reactsocial-dfbaf",
+    storageBucket: "reactsocial-dfbaf.appspot.com",
+    messagingSenderId: "606600039073",
+    appId: "1:606600039073:web:c2fb889ea1324016497ee0"
+  };
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/posts', (req, res) => {
-    admin
-        .firestore()
+    db
         .collection('posts')
         .orderBy('createdAt', 'desc')
         .get()
@@ -34,8 +46,7 @@ app.post('/post', (req, res) => {
             createdAt: new Date().toISOString()
         };
 
-        admin
-            .firestore()
+        db
             .collection('posts')
             .add(newPost)
             .then(doc => {
@@ -45,6 +56,37 @@ app.post('/post', (req, res) => {
                 res.status(500).json({ error: 'something went wrong' });
                 console.error(err);
             });
+});
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    // need to validate
+
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                return res.status(400).json({ handle: 'this handle is already taken' })
+            } else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
+
 });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
