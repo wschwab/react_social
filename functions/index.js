6cbdc8cc5aeac176d485e72f927dda40a6603484
wschwab <x@x.x>
2019-12-26 11:51:43 +0200

    added vaidation

--- functions/index.js
+++ functions/index.js
@@ -58,6 +58,17 @@ app.post('/post', (req, res) => {
             });
 });
 
+const isEmail = email => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+const isEmpty = string => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
 app.post('/signup', (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -66,7 +77,19 @@ app.post('/signup', (req, res) => {
         handle: req.body.handle,
     }
 
-    // need to validate
+    let errors = {};
+
+    if(isEmpty(newUser.email)) {
+        errors.email = "Email field required";
+    } else if(!isEmail(newUser.email)) {
+        errors.email = "Must be a valid email address";
+    }
+
+    if(isEmpty(newUser.password)) errors.password = "Password field required";
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = "Passwords do not match";
+    if(isEmpty(newUser.handle)) errors.handle = "Handle field required";
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
