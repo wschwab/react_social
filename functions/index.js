ae930e77ba0c1a4596c913c3fdcdbed3652c2f81
wschwab <x@x.x>
2019-12-26 12:17:06 +0200

    login

--- functions/index.js
+++ functions/index.js
@@ -128,4 +128,36 @@ app.post('/signup', (req, res) => {
 
 });
 
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = "Email field required";
+    if(!isEmail(user.email)) errors.email = "Invalid email";
+    if(isEmpty(user.password)) errors.password = "Password field required";
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({token});
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/wrong-password'){
+                return res.status(403).json({ general: 'Invalid credentials'});
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        })
+
+})
+
 exports.api = functions.region('europe-west1').https.onRequest(app);
